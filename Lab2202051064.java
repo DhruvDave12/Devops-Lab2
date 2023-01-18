004425a4833e6cb374c3e77a839dc3f9ad7a80dc
Dhruv Dave <davedhruv1201@gmail.com>
2023-01-18 16:19:05 +0530

    :tada: init repo to connect to jenkins and test

---
+++ Lab2202051064.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Lab2202051064{
+    public static void main(String[] args) {
+        System.out.println("Devops Lab Assignment 2");
+        System.out.println("Name: Dhruv Dave");
+        System.out.println("Roll No: 202051064");
+    }
+}
